643695c3617de8ad0e61dd7f037b6b3c4d2a1da7
shreeti <shreetis13@gmail.com>
2020-09-06 14:00:18 +0545

    ting models

--- apps/feed/models.py
+++ apps/feed/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ting(models.Model):
+  body = models.CharField(max_length=255)
+
+  created_by = models.ForeignKey(User, related_name='tings', on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    verbose_name = 'Ting'
+    verbose_name_plural = 'Tings'
+    ordering = ('-modified_at',)
