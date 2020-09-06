3855726cbc94bbe94fd8e3a3b5f14e14df0dd06d
shreeti <shreetis13@gmail.com>
2020-09-06 15:29:11 +0545

    get all from django on initial load

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Ting
+
 @login_required
 def feed(request):
-  return render(request, 'feed/feed.html')
+  userids = [request.user.id]
+
+  for tinger in request.user.tingerprofile.follows.all():
+    userids.append(tinger.user.id)
+
+  tings = Ting.objects.filter(created_by_id__in=userids)
+
+  return render(request, 'feed/feed.html', {'tings': tings})
--- tingtong/settings.py
+++ tingtong/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'apps.core',
     'apps.feed',
     'apps.tingerprofile',
